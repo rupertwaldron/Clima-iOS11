d23d1c6b357abea34aca5d33101c74752aa4fc5d
RuppyRup <rupert.waldron@yahoo.co.uk>
2017-10-31 15:58:27 +0000

    Collecting weather data in son

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -21,6 +21,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 
     //TODO: Declare instance variables here
     let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
 
     
     //Pre-linked IBOutlets
@@ -57,10 +58,12 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     func getWeatherData(url: String, parameters: [String : String]) {
         Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
             response in // returned data contained in response
+            // this is a closure
             if response.result.isSuccess {
                 print("Success we have weather data")
                 let weatherJSON : JSON = JSON(response.result.value!)
-                print(weatherJSON)
+                self.updateWeatherData(json: weatherJSON)
+                // need self as within a closure
             }
             else {
                 print("Error \(response.result.error!)")
@@ -79,7 +82,18 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
    
     
     //Write the updateWeatherData method here:
-    
+    func updateWeatherData(json : JSON) {
+        if let tempResult = json["main"]["temp"].double {
+            weatherDataModel.temperature = Int(tempResult - 273.15)
+            weatherDataModel.city = json["name"].stringValue
+            weatherDataModel.condition = json["weather"][0]["id"].intValue
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+        }
+        else {
+            cityLabel.text = "Weather Unavailable"
+        }
+        
+    }
 
     
     
@@ -105,6 +119,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         // stop checking when result is valid
         if location.horizontalAccuracy > 0 {
             locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil //stops into coming while stopping
             print("latitude = \(location.coordinate.latitude), Longitude = \(location.coordinate.longitude)")
             let latitude = String(location.coordinate.latitude)
             let longitude = String(location.coordinate.longitude)
